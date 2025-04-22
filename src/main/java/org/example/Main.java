0655b781f0bccae28a08921dbc1c8f9f9e08cd68
Ainur Otega <ainuralimjan@Mac.lan>
2025-04-22 02:28:33 -0700

    testing2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         while(true){
-            System.out.println("hello how are you doing?");
+            System.out.println("hello how are you doing?");System.out.println("hello how are you doing?");
+            System.out.println("git is boring");
         }
 
 

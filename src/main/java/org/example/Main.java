07bdf93b6a57ace080cb9b9df012f199dbed7bf0
codewiser116 <codewise.academy@gmail.com>
2025-04-21 20:31:09 -0500

    testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while(true){
+            System.out.println("hello how are you doing?");
+        }
     }
 }

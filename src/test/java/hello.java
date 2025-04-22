e84e0cec6d308c9bbda286e6dbf86f31be5648d3
kanykeiboron <k.boron93@gmail.com>
2025-04-21 20:36:00 -0500

    kaya commit

--- src/test/java/hello.java
+++ src/test/java/hello.java
@@ -2,5 +2,8 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("good evening!");
+        for (int i = 0; i < 2; i++) {
+            System.out.println("hello");
+        }
     }
 }

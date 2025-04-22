e41d4ee1510baf61ae1bec1f87c62bd170a266df
Yana <iana@Yanas-MBP.hsd1.il.comcast.net>
2025-04-21 20:42:37 -0500

    testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("good");
 
-        System.out.println("Hello world!");
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("hello, arigato, salam");
-        }
+        String name = "Yana";
+        int age = 11;
     }
 
 }

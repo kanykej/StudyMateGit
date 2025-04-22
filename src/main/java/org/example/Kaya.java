3fa791341586dac14558f3ff4ea22c79f99c389c
kanykeiboron <k.boron93@gmail.com>
2025-04-21 21:05:58 -0500

    k

--- src/main/java/org/example/Kaya.java
+++ src/main/java/org/example/Kaya.java
@@ -4,5 +4,6 @@ public class Kaya {
     public static void main(String[] args) {
         System.out.println("it's Kaya");
         System.out.println("kayaa");
+        System.out.println("k");
     }
 }

49e3c166db3a58b544e86377022b66cdb85cc4ac
kanykeiboron <k.boron93@gmail.com>
2025-04-21 20:33:25 -0500

    for loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello, arigato, salam");
+        }
     }
+
 }

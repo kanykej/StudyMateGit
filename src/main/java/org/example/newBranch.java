1ea8051af14b017210120d7a6dc2033faa5733f4
Yana <iana@Yanas-MBP.hsd1.il.comcast.net>
2025-04-21 20:39:57 -0500

    new Branch

---
+++ src/main/java/org/example/newBranch.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class newBranch {
+    public static void main(String[] args) {
+        System.out.println("good");
+
+        String name = "Yana";
+        int age = 11;
+    }
+}

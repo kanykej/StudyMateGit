4801332b95a98033e0cd8bf4d01336130787f898
Your Name <aigerimmukhtarkyzy@MacBook-Air-Aigerim.local>
2025-04-23 11:52:19 -0400

    changing the main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        while(true){
-            System.out.println("hello how are you doing?");System.out.println("hello how are you doing?");
-            System.out.println("git is boring");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Trying to change the main");
         }
 
 
